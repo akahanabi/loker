ed0477f9127a9dab5482f9f2d92f0b0859ec6e2a
akahanabi <rijal.16182@student.unsika.ac.id>
2019-03-15 10:22:20 +0700

    laman detail

--- views/site/detail.php
+++ views/site/detail.php
@@ -8,9 +8,9 @@ $this->title = 'Home';
 	<div class="container" style="background: #f1f1f1">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="single-page-header-inner">
+				<div class="">
 
-						<div class="col-md-3"><img src="themes/images/company-logo-01.png" alt="" style="width:100%"></div>
+						<div class="col-md-3" style="padding: 20px;"><img src="themes/images/company-logo-01.png" alt="" style="width:70%"></div>
 						<div class="col-md-9">
 							<h2>Judul Lowongan</h2>
 							<h4>Nama Perusahaan</h4>
@@ -26,16 +26,55 @@ $this->title = 'Home';
 			</div>
 		</div>
     </div>
-    <div class="container" style="background: rgb(250,100,50); padding: 10px;">
+    <div class="container" style="background: rgb(250,100,250); padding: 10px;">
         <div class="col-md-12">
-            <div class="col-md-3" style="padding: 2px">
-                <div style="background: white; padding: 2px">
+            <div class="col-md-4" style="padding: 5px">
+                <div style="background: white; padding: 5px; margin-bottom: 10px">
                     <h4>Ringkasan</h4>
+                    <ul>
+                        <li>Posisi</li>
+                        <li>Tipe pekerjaan</li>
+                        <li>Gaji</li>
+                        <li>Pengalaman</li>
+                        <li>Pendidikan Minimal</li>
+                        <li>Lokasi Kerja</li>
+                        <li>Tanggal Posting</li>
+                        <li>Berlaku Sampai</li>
+                    </ul>
+                </div>
+                <div style="margin-bottom: 10px">
+                    <button class="btn btn-success" style="width: 100%">Lamar Sekarang</button>
+                </div>
+                <div style="background: white; padding: 5px; margin-bottom: 10px">
+                    <h4>Lokasi</h4>
+                    <div style="height: 150px">
+					    Map berdasarkan latitude longitude
+				    </div>
                 </div>
             </div>
-            <div class="col-md-9" style="padding: 2px">
-                <div style="background: white; padding: 2px">
+            <div class="col-md-8" style="padding: 5px">
+                <div style="background: white; padding: 5px; margin-bottom: 10px;">
                     <h4>Deskripsi Pekerjaan</h4>
+                    <ul>
+                        <li>Responsible for the architectural design, development, and deployment of the OSS overall systems.</li>
+                        <li>Defines system solutions based on user/client needs, cost, and required integration with existing applications, systems, or platforms.</li>
+                        <li>Researches, identifies, selects, and tests technology products required for solution delivery.</li>
+                        <li>Establishes, implements, and documents the technology integration or migration strategies. The position requires staying on top of the industry trend and new technologies for the system architecture.</li>
+                        <li>Works on advanced, complex technical projects or business issues requiring state of the art technical or industry knowledge.</li>
+                        <li>Works autonomously. Goals are generally communicated in solution or project goal terms.</li>
+                        <li>May provide a leadership role for the work group through knowledge in the area of specialization</li>
+                    </ul>
+                    <hr>
+                    <h4>Persyaratan </h4>
+                    <ul>
+                        <li>S1 majoring Telecommunication Engineering, IT (Information Technology), Information System, Electro, Management Information, Programming.</li>
+                        <li>Min 5 years experience in System integration or Solution development.</li>
+                        <li>Good communication skills, team player, basic mobile communication knowledge,</li>
+                        <li>Preferred a woman</li>
+                        <li>Language skill (English)</li>
+                        <li>Discipline, innovative, willing to work under pressure & strong analytical skills</li>
+                        <li>Have strong competency in Software engineering, TCP/IP SNMP, CORBA, Scripting , SQL, Windows Server, Apache, IIS, ORACLE, JAVA scripts and HTML 5</li>
+                    </ul>
                 </div>
             </div>
         </div>
