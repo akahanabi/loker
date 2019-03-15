bfe9a25bd50ce93aa83390c2f825af3410e29185
akahanabi <rijal.16182@student.unsika.ac.id>
2019-03-15 17:00:34 +0700

    sketsa laman cari perusahaan 2.0

--- views/site/perusahaan.php
+++ views/site/perusahaan.php
@@ -9,8 +9,8 @@ $this->title = 'Perusahaan';
         <div class="col-md-4 sidenav sticky">
             <h4>Pilih Kriteria</h4><br>
             <form action="" method="">
-                <input type="text" class="form-control" placeholder="Masukan keyword" name="keyword"><br>
-                <select name="tipe" class="form-control">
+                <input type="text" class="form-control" placeholder="Masukan nama perusahaan" name="perusahaan"><br>
+                <!--<select name="tipe" class="form-control">
                     <option selected disabled>--Pilih Tipe Pekerjaan--</option>
                     <option value="waktu penuh">Waktu Penuh</option>
                     <option value="paruh waktu">Paruh Waktu</option>
@@ -25,10 +25,9 @@ $this->title = 'Perusahaan';
                     <option value="SMP">SMP</option>
                     <option value="SMA / SMK sederajat">SMA / SMK sederajat</option>
                     <option value="diploma">Diploma</option>
-                </select><br>
+                </select><br>-->
                 <input type="text" class="form-control" placeholder="Masukan lokasi" name="lokasi"><br>
-                <input type="number" class="form-control" placeholder="Masukan gaji minimum" name="gaji" min="0"><br>
-                <button class="btn btn-default" type="submit">Cari Lowongan</button>
+                <button class="btn btn-default" type="submit">Cari Perusahaan</button>
             </form>
         </div>
         <div class="col-md-8" style>
@@ -38,11 +37,9 @@ $this->title = 'Perusahaan';
                     <a href="" style="color: black"><i class="glyphicon glyphicon-sort-by-attributes"></i></a>
                     <select type="submit" name"sort">
                         <option></option>
-                        <option>Judul Lowongan</option>
-                        <option>Tanggal</option>
-                        <option>Perusahaan</option>
-                        <option>Gaji</option>
-                        <option>Lokasi</option>
+                        <option>Nama</option>
+                        <option>Jumlah Karyawan</option>
+                        <option>Rating</option>
                     </select>
                 </form>
             </div>
@@ -51,6 +48,7 @@ $this->title = 'Perusahaan';
                 <div  style="text-align: center; background: #f1f1f1; padding: 10px">
                     <img src="themes/images/company-logo-01.png" alt="" style="width:50%">
                     <h5>Nama Perusahaan</h4>
+                    <i class="glyphicon glyphicon-user"></i> 3982<br>
                     <i class="glyphicon glyphicon-star"></i>
                     <i class="glyphicon glyphicon-star"></i>
                     <i class="glyphicon glyphicon-star"></i>
